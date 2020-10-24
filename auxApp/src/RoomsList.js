a387f9103e0d5c7ab70a3375b72d83fbbfcae8dc
Tianyi Ma <tianyi.ma01@gmail.com>
2020-10-24 18:17:02 -0400

    add insideRoom page

--- auxApp/src/RoomsList.js
+++ auxApp/src/RoomsList.js
@@ -7,10 +7,10 @@ function RoomsList(props){
     <View style={{flex:1,}}>
         <Text style={{fontSize:40, marginTop:40}}>Listen Now: </Text>
         <ScrollView contentContainerStyle={styles.container}>
-          <Rooms roomName="Jazz" likesNum="200" thumbsUpNum="10"/>
-          <Rooms roomName="Soul" likesNum="300" thumbsUpNum="50"/>
-          <Rooms roomName="Groove" likesNum="1200" thumbsUpNum="500"/>
-          <Rooms roomName="Party Songs" likesNum="12345" thumbsUpNum="1000"/>
+          <Rooms roomName="Jazz" listenersNum="200" thumbsUpNum="10"/>
+          <Rooms roomName="Soul" listenersNum="300" thumbsUpNum="50"/>
+          <Rooms roomName="Groove" listenersNum="1200" thumbsUpNum="500"/>
+          <Rooms roomName="Party Songs" listenersNum="12345" thumbsUpNum="1000"/>
         </ScrollView>
         <View style={styles.bottomButton}>
           <Button name="Listen"/>
@@ -23,7 +23,7 @@ function RoomsList(props){
 function Rooms(props){
   return (
     <View style={styles.roomsStyle}>
-      <Text style={{fontSize:20}}>{props.roomName}      <Image source={{uri:"http://www.integriward.com/wp-content/uploads/2015/04/people-icon.png"}} style={styles.people}/>{props.likesNum}  <Image source={{uri:"https://emojis.wiki/emoji-pics/twitter/thumbs-up-twitter.png"}} style={styles.likes}/>{props.thumbsUpNum}</Text>   
+      <Text style={{fontSize:20}}>{props.roomName}      <Image source={{uri:"http://www.integriward.com/wp-content/uploads/2015/04/people-icon.png"}} style={styles.people}/>{props.listenersNum}  <Image source={{uri:"https://emojis.wiki/emoji-pics/twitter/thumbs-up-twitter.png"}} style={styles.likes}/>{props.thumbsUpNum}</Text>   
     </View>
   )
 }
--- auxApp/src/insideRoom.js
+++ auxApp/src/insideRoom.js
@@ -1,20 +1,62 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Image, Button } from 'react-native';
-import RoomsList from './RoomsList'
 
+// props i.e.: {username: "John Smith"}
 function InsideRoom(props){
+
+  const[currMusicName, setCurrMusicName] = useState("jazz song 1")
+  const[currMusician, setCurrMusician] = useState("jazz musician 1")
+  const[listenersNum, setListenersNum] = useState(0)
+  const[likesNum, setLikesNum] = useState(0)
+
+  return(
+    <View style={{flex:1}}>
+      <Text style={styles.headline}>{props.username}</Text>
+      <View contentContainerStyle={styles.container}>
+        <Text style={styles.subLine}>You are listening to:</Text>
+        <Text style={styles.subLine}>{currMusicName} by {currMusician}</Text>
+      </View>
+      <View style={styles.sideToSide}>
+        <Info listenersNum={listenersNum} likesNum={likesNum}/>
+        <Button title="Like"/>
+      </View>
+      <View style={styles.sideToSide}>
+          <Button title="Listen"/>
+          <Button title="DJ"/>
+      </View>
+    </View>
+    )
+}
+
+function Info(props){
   return(
     <View>
-      <Text style={styles.headline}>{props.username}</Text>
+      <Text style={styles.subLine}>{props.listenersNum} listeners </Text>
+      <Text style={styles.subline}>{props.likesNum} likes </Text>
     </View>
     )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
   headline: {
     fontSize:40, 
     marginTop:40,
-  }
+  },
+
+  subLine: {
+    fontSize:20,
+  },
+
+  sideToSide: {
+    flexDirection: "row",
+  },
 });
+
 
 export default InsideRoom;
