533cf43196d3dc2b1d17978bd74eb16fce95ce30
Tianyi Ma <tianyi.ma01@gmail.com>
2020-12-03 01:03:31 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Button } from 'react-native';
-import RoomsList from './RoomsList'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import SignInPage from './src/signInPage';
+import RoomsList from './src/RoomsList';
+import InsideRoom from './src/insideRoom';
+import StartDJ from './src/startDJ';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <RoomsList/>
-  )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="GoIntoARoom">
+        <Stack.Screen name="SignInPage" component={SignInPage} />
+        <Stack.Screen name="RoomsList" component={RoomsList} />
+        <Stack.Screen name="InsideRoom" component={InsideRoom} />
+        <Stack.Screen name="StartDJ" component={StartDJ} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-function StartDJ(props){
-  return(
-    <View>
-      <Text style={styles.headline}>{props.username}</Text>
-    </View>
-    )
-}
-
-const styles = StyleSheet.create({
-  headline: {
-    fontSize:40, 
-    marginTop:40,
-  }
-});
+"0 mod 15", "1 mod 15", "2 mod 15", "3 mod 15", "4 mod 15", "5 mod 15", "6 mod 15", "7 mod 15", 
+                                            "8 mod 15", "9 mod 15", "10 mod 15", "11 mod 15", "12 mod 15", "13 mod 15", "14 mod 15"
---
+++ src/RoomsList.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Image, TouchableHighlight } from 'react-native';
+import Footer from "./footer";
+import Header from "./header";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function RoomsList(props){
+  return(
+    <View style={styles.container}>
+        <Header roomName="TEST"/>
+          <RoomsContainer {...props}/>
+      <Footer {...props}/>
+      </View>
+    )
+}
+
+function Rooms(props){
+  return (
+  <TouchableHighlight
+    activeOpacity={0.6}
+    underlayColor="#DDDDDD"
+    onPress={() => props.navigation.navigate("InsideRoom")}>
+
+    <View style={styles.roomsStyle}>
+      <Text style={styles.roomsName}>{props.roomName}</Text> 
+      <View style={styles.interaction}>
+        <View style={styles.nPeople}>     
+          <Image source={{uri:"https://thumbs.dreamstime.com/b/person-icon-flat-style-man-symbol-person-icon-flat-style-man-symbol-isolated-white-background-simple-people-abstract-icon-118611127.jpg"}} style={styles.people}/>
+          <Text style={styles.peopleText}>{props.likesNum} </Text>
+        </View>
+        <View style={styles.nThumbsUp}>
+          <Image source={{uri:"https://emojis.wiki/emoji-pics/twitter/thumbs-up-twitter.png"}} style={styles.likes}/>
+          <Text style={styles.likesText}>{props.thumbsUpNum}</Text>
+        </View>   
+      </View>
+    </View>
+    </TouchableHighlight>
+  )
+}
+
+function RoomsContainer(props){
+  return(
+    <ScrollView style={styles.roomsContainer}>
+      <Rooms roomName="Jazz" likesNum="200" thumbsUpNum="10" {...props}/>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+
+  roomsContainer:{
+    marginLeft:5,
+    width:'95%',
+  },
+
+  roomsStyle: {
+    flexDirection:'row',
+    height: 50,
+    width: '100%',
+    backgroundColor: "#292929",
+    padding: 10,
+    marginBottom: '5%',
+    borderRadius:30,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+  },
+
+  roomsName: {
+    color:'#f9f9f9',
+    marginLeft: 10,
+  },
+
+  interaction:{
+    width:'40%',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+  },
+
+  nPeople:{
+    flexDirection:'row',
+    textAlign:'center',
+    width:'50%',
+    alignItems:'center',
+  },
+
+  peopleText:{
+    color:'#f9f9f9',
+    paddingLeft:5,
+  },
+
+  nThumbsUp:{
+    flexDirection:'row',
+    textAlign:'center',
+    width:'50%',
+    alignItems:'center'
+  },
+
+  likesText:{
+    color:'#f9f9f9',
+    paddingLeft:5,
+  },
+
+  people: {
+    width:10,
+    height:10      ,
+  },
+
+  likes: {
+    width: 10,
+    height: 10,
+  },
+});
+
+
+export default RoomsList;
